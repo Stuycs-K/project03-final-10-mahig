d9892cb61445adb2f06518a40faadcfe65c092ca
Golam Mahi <gmahi40@stuy.edu>
2024-01-10 15:27:21 -0500

    working on readSchedule function

--- schedule.c
+++ schedule.c
@@ -1,6 +1,30 @@
 #include "schedule.h"
 #define FILENAME "schedule.txt"
+#define LENGTH 20
+
+
+char** readSchedule(){
+    char activities = calloc(LENGTH, sizeof(&char));
+
+    int log = open(FILENAME, O_RDONLY, 0);
+
+  if (log < 0)  {  
+    perror("open");  
+    exit(1);  
+}  
+for(int i = 0; i < LENGTH; i++){
+    int stopRead; 
+    
+    ssize_t readed = read(log, activities[i], )
+}
+    return activities;
+}
+
+void calculateTime(){
+    
+}
 
 void displaySchedule(){
-
+    printf("Entry Format : Activity, Time(24Hour)");
 }
+
